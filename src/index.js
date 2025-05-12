7c87916743472511fca1cbbbf5add6ea0122416d
AngelMonoestereo <angel.thevinylpourclub.com>
2025-05-12 14:59:40 -0400

    Fix Cart.jsx and update functionality

--- src/index.js
+++ src/index.js
@@ -20,11 +20,9 @@ library.add(faBars, faShoppingCart, faTimes, faBookOpen, faTags, faStar, faBolt,
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 
